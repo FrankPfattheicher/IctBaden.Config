34e3e2f613736697aec8a7765a4f466553845fe6
Frank Pfattheicher <fpf@ict-baden.de>
2019-11-24 19:40:58 +0100

    Refacturing

--- IctBaden.Config/Schema/SchemaFromProfile.cs
+++ IctBaden.Config/Schema/SchemaFromProfile.cs
@@ -7,60 +7,60 @@ using IctBaden.Framework.IniFile;
 
 namespace IctBaden.Config.Schema
 {
-  public class SchemaFromProfile
-  {
-    public static ConfigurationUnit Create(string fileName)
+    public class SchemaFromProfile
     {
-      var iniFile = new Profile(fileName);
-
-      var root = new ConfigurationUnit
-      {
-        NamespaceProvider = "file://" + fileName,
-        Id = Path.GetFileNameWithoutExtension(fileName),
-        DisplayName = Path.GetFileName(fileName),
-        DataType = TypeCode.Object,
-        Description = fileName,
-        DisplayImage = "far fa-file"
-      };
-
-      foreach (var section in iniFile.Sections)
-      {
-        var sectionUnit = new ConfigurationUnit
+        public static ConfigurationUnit Create(string fileName)
         {
-          Parent = root,
-          Id = section.Name,
-          DisplayName = section.Name,
-          DataType = TypeCode.Object,
-          DisplayImage = "far fa-file-alt"
-        };
-        root.Children.Add(sectionUnit);
+            var iniFile = new Profile(fileName);
 
-        foreach (var keyUnit in section.Keys.Select(key => new ConfigurationUnit
-        {
-          Parent = sectionUnit,
-          Id = key.Name,
-          DisplayName = key.Name,
-          DataType = DetectTypeFromValue(key.StringValue)
-        }))
-        {
-          sectionUnit.Children.Add(keyUnit);
+            var root = new ConfigurationUnit
+            {
+                NamespaceProvider = "file://" + fileName,
+                Id = Path.GetFileNameWithoutExtension(fileName),
+                DisplayName = Path.GetFileName(fileName),
+                DataType = TypeCode.Object,
+                Description = fileName,
+                DisplayImage = "far fa-file"
+            };
+
+            foreach (var section in iniFile.Sections)
+            {
+                var sectionUnit = new ConfigurationUnit
+                {
+                    Parent = root,
+                    Id = section.Name,
+                    DisplayName = section.Name,
+                    DataType = TypeCode.Object,
+                    DisplayImage = "far fa-file-alt"
+                };
+                root.Children.Add(sectionUnit);
+
+                foreach (var keyUnit in section.Keys.Select(key => new ConfigurationUnit
+                {
+                    Parent = sectionUnit,
+                    Id = key.Name,
+                    DisplayName = key.Name,
+                    DataType = DetectTypeFromValue(key.StringValue)
+                }))
+                {
+                    sectionUnit.Children.Add(keyUnit);
+                }
+            }
+
+            return root;
         }
-      }
 
-      return root;
+        private static TypeCode DetectTypeFromValue(string stringValue)
+        {
+            var looksLikeDouble = new Regex(@"^[+-]?[0-9]*\.[0-9]+$");
+            if (looksLikeDouble.IsMatch(stringValue))
+                return TypeCode.Double;
+
+            var looksLikeInteger = new Regex(@"^[+-]?[0-9]+$");
+            if (looksLikeInteger.IsMatch(stringValue))
+                return TypeCode.Int64;
+
+            return TypeCode.String;
+        }
     }
-
-    private static TypeCode DetectTypeFromValue(string stringValue)
-    {
-      var looksLikeDouble = new Regex(@"^[+-]?[0-9]*\.[0-9]+$");
-      if (looksLikeDouble.IsMatch(stringValue))
-        return TypeCode.Double;
-
-      var looksLikeInteger = new Regex(@"^[+-]?[0-9]+$");
-      if (looksLikeInteger.IsMatch(stringValue))
-        return TypeCode.Int64;
-      
-      return TypeCode.String;
-    }
-  }
 }
