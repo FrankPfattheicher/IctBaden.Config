482898028af688bca57f020234b5fec15ebaac41
Frank Pfattheicher <fpf@ict-baden.de>
2020-03-16 18:05:17 +0100

    Minor refacturing

--- IctBaden.Config/Unit/ConfigurationUnitPropertyProvider.cs
+++ IctBaden.Config/Unit/ConfigurationUnitPropertyProvider.cs
@@ -8,35 +8,35 @@ namespace IctBaden.Config.Unit
 {
     public class ConfigurationUnitPropertyProvider : IPropertyProvider
     {
-        private readonly ConfigurationUnit unit;
+        private readonly ConfigurationUnit _unit;
 
         public ConfigurationUnitPropertyProvider(ConfigurationUnit cfgUnit)
         {
-            unit = cfgUnit;
+            _unit = cfgUnit;
         }
 
         #region IPropertyProvider Members
 
         public List<T> GetAll<T>()
         {
-            return (from property in unit.Children where property.IsProperty && (property.GetType() == typeof(T)) select property.GetValue<T>()).ToList();
+            return (from property in _unit.Children where property.IsProperty && (property.GetType() == typeof(T)) select property.GetValue<T>()).ToList();
         }
 
         public T Get<T>(string key)
         {
-            var prop = unit.GetUnitById(key);
+            var prop = _unit.GetUnitById(key);
             return prop.GetValue<T>();
         }
 
         public T Get<T>(string key, T defaultValue)
         {
-            var prop = unit.GetUnitById(key);
+            var prop = _unit.GetUnitById(key);
             return prop.IsEmpty ? defaultValue : prop.GetValue(defaultValue);
         }
 
         public void Set<T>(string key, T newValue)
         {
-            var prop = ConfigurationUnit.GetProperty(unit, key);
+            var prop = ConfigurationUnit.GetProperty(_unit, key);
             prop.SetValue(newValue);
         }
 
@@ -47,7 +47,7 @@ namespace IctBaden.Config.Unit
 
         public bool Contains(string key)
         {
-            return !unit.GetUnitById(key).IsEmpty;
+            return !_unit.GetUnitById(key).IsEmpty;
         }
 
         #endregion
