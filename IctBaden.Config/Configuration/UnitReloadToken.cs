90fd7eec4b3500f346aa31f5e18409c70bcaebef
Frank Pfattheicher <fpf@ict-baden.de>
2021-06-29 18:14:09 +0200

    comments

--- IctBaden.Config/Configuration/UnitReloadToken.cs
+++ IctBaden.Config/Configuration/UnitReloadToken.cs
@@ -1,11 +1,13 @@
 using System;
 using IctBaden.Config.Unit;
 using Microsoft.Extensions.Primitives;
+// ReSharper disable UnassignedGetOnlyAutoProperty
 
 namespace IctBaden.Config.Configuration
 {
     public class UnitReloadToken : IChangeToken
     {
+        // ReSharper disable once NotAccessedField.Local
         private readonly ConfigurationUnit _unit;
 
         public UnitReloadToken(ConfigurationUnit cfgUnit)
